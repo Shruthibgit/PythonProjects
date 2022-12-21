5695706d583b3435cd31b817e90e2d43e24ff854
Shruthi B <bshruthi8659@gmail.com>
2022-12-21 23:25:16 +0530

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,4 +1,4 @@
-weight = float(input("Enter your weight in kg"))
-hight = float(input('Enter your hight in meters'))
-bmi = float(weight / (hight*hight))
+weight = float(input("Enter your weight in kg: "))
+hight = float(input("Enter your hight in meters: "))
+bmi = weight / (hight*hight)
 print("Your BMI is: ", bmi)
