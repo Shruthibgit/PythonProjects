0ae89c1da6d3c927da63403e7a43da6f50747b38
Shruthi B <bshruthi8659@gmail.com>
2022-12-22 13:26:23 +0530

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,4 +1,14 @@
-weight = float(input("Enter your weight in kg: "))
-hight = float(input("Enter your hight in meters: "))
-bmi = weight / (hight*hight)
-print("Your BMI is: ", bmi)
+weight = 0
+height = 0
+while True:
+    try:
+        weight = float(input("Enter you weight: "))
+        height = float(input("Enter you hight: "))
+    except:
+        print("Please enter the valid data!!!!")
+        continue
+    else:
+        bmi = weight/(height**2)
+        print("You BMI is : ", bmi)
+        break
+print("Thank you for using BMI calculator!!")
