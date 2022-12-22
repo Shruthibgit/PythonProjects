c8cd3e4896d66d7f2a0f06b565fb362b0a632171
Shruthi B <bshruthi8659@gmail.com>
2022-12-22 23:31:04 +0530

    Add files via upload

---
+++ BFP.py
@@ -0,0 +1,31 @@
+weight = 0
+height = 0
+age = 0
+
+def bfp_calculator():
+    height_meters = height * 0.3048  # converting to meters
+    bmi = weight / (height_meters ** 2)
+    print("Your BMI is ", round(bmi,2))
+
+    if gen == 'M' or gen == 'm':
+        bfp = (1.20*bmi)+(0.23*age)-16.2
+        print("Your Body fat percentage = ", round(bfp,2))
+
+    elif gen == 'F' or 'f':
+        bfp = (1.20 * bmi) + (0.23 * age) - 5.4
+        print("Your Body fat percentage = ", round(bfp,2))
+
+while True:
+ try:
+     weight = float(input("Enter you weight in kg: "))
+     height = float(input("Enter you height in inches: "))
+     age = int(input("Enter your age: "))
+     gen = input("Enter your gender M/F: ")
+ except:
+     print("Please enter the valid data!!!!")
+     continue
+ else:
+     bfp_calculator()
+ break
+
+print("Thank you for using BMI calculator!!")
